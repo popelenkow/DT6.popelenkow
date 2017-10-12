ad3f3afd0da6ddff16a4dddd9c9848e351dabddb
Popelenkov Vladlen <popelenkow@yandex.ru>
2017-10-12 23:01:37 +0700

    Added final implementation of clojure1

---
+++ clojure1/src/clojure1/core.clj
@@ -0,0 +1,20 @@
+(ns clojure1.core)
+
+(defn multiply [first acc]
+  (for [ch first
+        str acc
+				:when (not (= (.charAt ch 0) (.charAt str 0)))]
+				(.concat ch str)))
+
+(defn superMultiply [N arr]
+  "N is length, arr is dictionary"
+  (loop [i N
+         first arr
+         acc arr]
+    (if (pos? (dec i))
+      (recur (dec i) first (multiply first acc))
+      acc)))
+  
+(defn -main [& args]
+  (println
+    (superMultiply 3 ["^" "\u0040" "x"])))
---
+++ clojure1/test/clojure1/core_test.clj
@@ -0,0 +1,42 @@
+(ns clojure1.core-test
+  (:require [clojure.test :refer :all]
+            [clojure1.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (=
+          (superMultiply 4 ["a" "b" "c"])
+          (list
+           "abab"
+					 "abac"
+					 "abca"
+					 "abcb"
+					 "acab"
+					 "acac"
+					 "acba"
+					 "acbc"
+					 "baba"
+					 "babc"
+					 "baca"
+					 "bacb"
+					 "bcab"
+					 "bcac"
+					 "bcba"
+					 "bcbc"
+					 "caba"
+					 "cabc"
+					 "caca"
+					 "cacb"
+					 "cbab"
+					 "cbac"
+					 "cbca"
+					 "cbcb")))))
+
+(deftest b-test
+  (testing "FIXME, I fail."
+    (is (=
+          (superMultiply 1 ["3" "1" "2"])
+          (list
+           "3"
+					 "1"
+					 "2")))))
