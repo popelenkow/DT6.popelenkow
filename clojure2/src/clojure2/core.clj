4b9a2742ff6f2675c4c79cf0f372e86dd45a9eaf
Popelenkov Vladlen <popelenkow@yandex.ru>
2017-10-20 09:57:23 +0700

    Added partical implementation of clojure2

---
+++ clojure2/src/clojure2/core.clj
@@ -0,0 +1,44 @@
+(ns clojure2.core)
+
+(def Step (double 1/1000))
+
+(defn- GetCountStepCos
+  [x]
+  (double (int 
+            (/
+              (double x)
+              Step))))
+
+
+(defn- ICos
+  [i]
+  (*
+    Step
+    (double 1/2)
+    (+
+      (Math/cos (* Step i))
+      (Math/cos (* Step (dec i))))))
+
+(defn- _IntegrateCos
+  [count]
+  (loop [i count
+         acc (double 0)]
+    (if (pos? i)
+      (recur (dec i) (+ acc (ICos i)))
+      acc)))
+
+(def _IntegrateCos-memo
+  (memoize _IntegrateCos))
+  
+(defn IntegrateCos
+  [x]
+  (_IntegrateCos (GetCountStepCos x)))
+
+(defn IntegrateCos-memo
+  [x]
+  (let [i (GetCountStepCos x)]
+    (_IntegrateCos-memo i)))
+
+(defn -main [& args]
+  (println
+    (IntegrateCos 1)))
---
+++ clojure2/test/clojure2/core_test.clj
@@ -0,0 +1,35 @@
+(ns clojure2.core-test
+  (:require [clojure.test :refer :all]
+            [clojure2.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is
+      (>
+        (* 7 Step Step)
+        (Math/abs
+          (-
+            (Math/sin 7)
+            (IntegrateCos 7)))))))
+
+(deftest b-test
+  (testing "FIXME, I fail."
+    (is
+      (>
+        (* 1000.1 Step Step)
+        (Math/abs
+          (-
+            (Math/sin 1000.1)
+            (IntegrateCos 1000.1)))))))
+
+(deftest c-test
+  (testing "FIXME, I fail."
+    (is
+      (>
+        (* 0.001 Step Step)
+        (Math/abs
+          (-
+            (Math/sin 0.001)
+            (IntegrateCos 0.001)))))))
+
+
